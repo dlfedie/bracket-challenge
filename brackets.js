57bbe4c35aa481585116a72f9235eb1ef17aec41
Dustin Fedie <dlfedie@gmail.com>
2019-12-23 15:15:00 -0600

    got it to work, finally

--- brackets.js
+++ brackets.js
@@ -1,7 +1,7 @@
 function checkBrackets(stringOfBrackets) {
 
     // console.log('get anything now?');
-    
+
     // get an input of a string of ( ) [ ] or { }
     // make sure they close properly
     // return yes or no depending on if they do or not
@@ -15,46 +15,52 @@ function checkBrackets(stringOfBrackets) {
 
     for (let char of stringOfBrackets) {
         // console.log('getting here?');
-        
+
         if (char === '(' || char === '{' || char === '[') {
             // console.log('pushing the opening brace');
-            
+
             stack.push(char);
         }
         else if (char === ')' || char === '}' || char === ']') {
             // console.log('any closing braces?');
-            
-            
+
+
             if (stack.length === 0) {
-                console.log('started with a closing brace, or too few opening braces!');
+                // console.log('started with a closing brace, or too few opening braces!');
                 stringToReturn = 'NO';
                 break;
             }
             let pairCheckerArray = [];
             pairCheckerArray.push(stack.pop());
-            
-            
+
+
             pairCheckerArray.push(char);
 
-            console.log('pairCheckerArray: ', pairCheckerArray);
-            console.log(goodPairs);
-            
+            // console.log('pairCheckerArray: ', pairCheckerArray);
+            // console.log(goodPairs);
+            let matches = 0;
 
             for (let i = 0; i < goodPairs.length; i++) {
-                console.log('are we ever hitting this fucking thing?');
+                // console.log('are we ever hitting this fucking thing?', pairCheckerArray, goodPairs[i]);
                 
-                if (goodPairs[i] === pairCheckerArray) {
-                    console.log('found a match!');
+
+                if (goodPairs[i][0] === pairCheckerArray[0] && goodPairs[i][1] === pairCheckerArray[1]) {
+                    // console.log('found a match!');
+                    matches++;
                     break;
                 }
                 
-                // otherwise, if it gets through the loop w/o a match, break it and string is NO
-                // stringToReturn = 'NO';
-                // return stringToReturn;
             }
-            pairCheckerArray = [];
             
+            if (matches === 0) {
+                // console.log('no match');
+                // otherwise, if it gets through the loop w/o a match, break it and string is NO
+                stringToReturn = 'NO';
+                return stringToReturn;
+            }
+
         }
+
 
 
         if (stack.length === 0) {
@@ -67,11 +73,12 @@ function checkBrackets(stringOfBrackets) {
 }
 
 
-console.log(checkBrackets('(){}[]'));
-console.log(checkBrackets('({}[]'));
-console.log(checkBrackets('}[]'));
-console.log(checkBrackets('{}}[]'));
-console.log(checkBrackets('{)[]'));
-console.log(checkBrackets('{([])}'));
+console.log(checkBrackets('(){}[]'), ' - should be YES');
+console.log(checkBrackets('({}[]'), ' - should be NO');
+console.log(checkBrackets('}[]'), ' - should be NO');
+console.log(checkBrackets('{}}[]'), ' - should be NO');
+console.log(checkBrackets('{)[]'), ' - should be NO');
+console.log(checkBrackets('{([])}'),  ' - should be YES');
+console.log(checkBrackets('{([([]{}())])}'),  ' - should be YES');
 
 
